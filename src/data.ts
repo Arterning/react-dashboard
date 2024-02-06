4e646ce78f0197f0c2c5c0312e83011765cb6efa
Arterning <ning.huang.ridson@gmail.com>
2024-02-06 15:16:13 +0800

    feat: Remove useless route

--- src/data.ts
+++ src/data.ts
@@ -33,18 +33,6 @@ export const menu = [
         url: "/products",
         icon: "product.svg",
       },
-      {
-        id: 3,
-        title: "Orders",
-        url: "/orders",
-        icon: "order.svg",
-      },
-      {
-        id: 4,
-        title: "Posts",
-        url: "/posts",
-        icon: "post2.svg",
-      },
     ],
   },
   {
